1c193e47bb829b31988d3e87c759be11431a164b
Maritn Lachev <martinlachev1@gmail.com>
2020-08-19 17:06:09 +0300

    eTKu1suy: Adding coordinator pattern in order to navigate around the app

---
+++ BreakingBadCharacters/SupportFiles/Coordinators/MainCoordinator.swift
@@ -0,0 +1,49 @@
+
+import UIKit
+
+class MainCoordinator: NSObject, Coordinator, UINavigationControllerDelegate {
+    
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        navigationController.delegate = self
+    
+        startCharactersScreen()
+    }
+    
+    func startCharactersScreen() {
+        let child = CharactersScreenCoordinator(navigationController: navigationController)
+        childCoordinators.append(child)
+        child.parentCoordinator = self
+        
+        child.start()
+    }
+    
+    func childDidFinish(_ child: Coordinator?) {
+        for (index, coordinator) in childCoordinators.enumerated() {
+            if coordinator === child {
+                childCoordinators.remove(at: index)
+                break
+            }
+        }
+    }
+    
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
+            return
+        }
+        
+        if navigationController.viewControllers.contains(fromViewController) {
+            return
+        }
+        
+        if let charactersViewController = fromViewController as? CharactersViewController {
+            childDidFinish(charactersViewController.coordinator)
+        }
+    }
+}
